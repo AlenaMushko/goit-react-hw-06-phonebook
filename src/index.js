fd3c092f478a8749dfb38a2d94a582944772ecaf
alena <evstafienko_a@ukr.net>
2023-03-04 18:00:28 +0200

    complited

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import './styles/index.css';
 import './styles/theme.jsx';
 import { store, persistor } from 'redux/store';
 import { Provider } from 'react-redux';
-// import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <Provider store={store}>
-    {/* <PersistGate loading={null} persistor={persistor}> */}
-      <App />
-    {/* </PersistGate> */}
-  </Provider>
-  // </React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
