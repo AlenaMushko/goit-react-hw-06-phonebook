112bdb7851711e55906ef7f99213dd2a69eb6a2e
alena <evstafienko_a@ukr.net>
2023-03-04 17:52:20 +0200

    <PersistGate  in index.js

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </PersistGate>
-  </React.StrictMode>
+  </Provider>
+  // </React.StrictMode>
 );
