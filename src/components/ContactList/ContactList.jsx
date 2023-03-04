2bae6abec293bb25febfb896771bbd12d6235d81
alena <evstafienko_a@ukr.net>
2023-03-04 18:14:57 +0200

    contacts?.

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -11,8 +11,7 @@ export const ContactList = () => {
 
   const visibleContacts = () => {
     const normolizeFilter = filter.toLowerCase();
-    console.log(contacts);
-    return contacts.find(contact =>
+    return contacts?.filter(contact =>
       contact.name.toLowerCase().includes(normolizeFilter)
     );
   };
